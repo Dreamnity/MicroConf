2341f3ff95a63e2400275ba9a0539e7e4a634a5b
superdinmc <superdinmc@gmail.com>
2023-11-22 17:20:44 +0700

    fix

--- index.js
+++ index.js
@@ -17,23 +17,23 @@ function stringifyCompactConfig(configObject) {
 }
 const regexes = {
   value: /^(?<key>[^:\)\()]+)\:(?<value>[^\),]+)\,/,
-  object: /^(?<key>[^:]+)\((?<object>[^\)]+:[^\)]+\))\),/
+  object: /(?<key>[^:]*?)\((?<object>[^\)]+|[^\)]+\))\),/
 }
-function parseCompactConfig(string) {
+function parseCompactConfig(string,debug) {
   var result = {};
   var compactString = (string+'').replace(/\\(.)/g,(a,b)=>'ESCAPED{'+Buffer.from(b).toString('hex')+'}');
   let count=20,match;
   if (typeof compactString !== 'string') throw new Error('Not a string.');
-  console.log('++Start',string)
+  if(debug) console.log('++Start',string)
   if(!compactString.endsWith(',')) compactString += ','
   while (compactString.length > 0 && count >= 0) {
-    console.log('  '+compactString)
+    if(debug) console.log('  '+compactString)
     count--;
     match = compactString.match(regexes.value);
     compactString = compactString.replace(regexes.value, '');
     if (match) {
       count++;
-      console.log('++Value',match[0])
+      if(debug) console.log('++Value',match[0])
       //console.log(match);
       let value = match.groups.value.replace(/ESCAPED{([a-zA-Z0-8]{1,8})}/g, (a, b) => '\\' + Buffer.from(b, 'hex').toString());
       try {
@@ -48,13 +48,13 @@ function parseCompactConfig(string) {
     if (match) {
       count++;
       //console.log(match[0])
-      console.log('++Recursing',match[0])
+      if(debug) console.log('++Recursing',match[0])
       result[match.groups.key] = parseCompactConfig(match.groups.object)
       continue;
     }
     compactString = compactString.slice(1)
   }
-  console.log('++Exit',string)
+  if(debug) console.log('++Exit',string)
   if (count < 0) throw new Error('Infinite loop detected');
   return result
 }
