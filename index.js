861b5c4b58c48b9151905f31ae3ba0f666997fdb
superdinmc <superdinmc@gmail.com>
2023-11-22 16:51:44 +0700

    Forgot

--- index.js
+++ index.js
@@ -16,24 +16,24 @@ function stringifyCompactConfig(configObject) {
 	return stringifyObject(configObject);
 }
 const regexes = {
-  value: /^(?<key>[^:\)\()]+)\|(?<value>[^\),]+)\,/,
-  object: /^(?<key>[^:]+)\((?<object>[^\)]+|[^\)]+\))\),/
+  value: /^(?<key>[^:\)\()]+)\:(?<value>[^\),]+)\,/,
+  object: /^(?<key>[^:]+)\((?<object>[^\)]+:[^\)]+\))\),/
 }
 function parseCompactConfig(string) {
   var result = {};
   var compactString = (string+'').replace(/\\(.)/g,(a,b)=>'ESCAPED{'+Buffer.from(b).toString('hex')+'}');
   let count=20,match;
   if (typeof compactString !== 'string') throw new Error('Not a string.');
-  //console.log('++Start',string)
+  console.log('++Start',string)
   if(!compactString.endsWith(',')) compactString += ','
   while (compactString.length > 0 && count >= 0) {
-    //console.log('  '+compactString)
+    console.log('  '+compactString)
     count--;
     match = compactString.match(regexes.value);
     compactString = compactString.replace(regexes.value, '');
     if (match) {
       count++;
-      //console.log('++Value',match[0])
+      console.log('++Value',match[0])
       //console.log(match);
       let value = match.groups.value.replace(/ESCAPED{([a-zA-Z0-8]{1,8})}/g, (a, b) => '\\' + Buffer.from(b, 'hex').toString());
       try {
@@ -48,13 +48,13 @@ function parseCompactConfig(string) {
     if (match) {
       count++;
       //console.log(match[0])
-      //console.log('++Recursing',match[0])
+      console.log('++Recursing',match[0])
       result[match.groups.key] = parseCompactConfig(match.groups.object)
       continue;
     }
     compactString = compactString.slice(1)
   }
-  //console.log('++Exit',string)
+  console.log('++Exit',string)
   if (count < 0) throw new Error('Infinite loop detected');
   return result
 }
@@ -63,6 +63,7 @@ module.exports = {
   parse: parseCompactConfig,
   stringify: stringifyCompactConfig
 }
+console.log(parseCompactConfig('result:{author\:5UP3R_D1N},options(headers:(Content-Type:application/json))'))
 /*
 const configObject = {
   server: {
